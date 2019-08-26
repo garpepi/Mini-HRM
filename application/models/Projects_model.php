3e21e2e73d5f22989a4f1d18dc113d913b1a1b13
Garpepi Hanief Aotearoa <garpepi.aotearoa@gmail.com>
2019-08-26 13:22:21 +0700

    fixing employee id existing on adding new projects module (allowance sector)

--- application/models/Projects_model.php
+++ application/models/Projects_model.php
@@ -21,28 +21,36 @@ class Projects_model extends CI_Model {
 		$this->load->model('allowance_model', 'am');
 		$this->load->model('attendance_timing_model', 'atm');
 		$this->load->model('config_model', 'cm');
+		$this->load->model('Employee_position_model', 'ep');
 		$data['user_c'] = $this->session->userdata('logged_in_data')['id'];
+		$employee_position_ids = $this->ep->get_emp_position(array('status'=>'active'));
 		$this->db->trans_begin();
           if($this->db->insert('projects',array('name' => $data['name'], 'client_id' => $data['client_id'], 'user_c'=> $data['user_c'])) ){
 			  $data['project_id'] = $this->db->insert_id();
 			  $data['client'] = $data['client_id'];
-			  if($this->am->insert_allowance($data)){
-				  if($this->atm->insert_attendance_timing($data)){
-					  if($this->cm->insert_config_qatracker($data)){
-						$this->db->trans_commit();
-						return TRUE;					  						  
-					  }else{
-						$this->db->trans_rollback();
-						return FALSE;
-					  }
+			  foreach($employee_position_ids as $employee_position_id)
+			  {
+				  $data['employee_position_id'] = $employee_position_id['id'];
+				  if(!$this->am->insert_allowance($data))
+				  {
+					$this->db->trans_rollback();
+					return FALSE;
+					exit();
+				  }
+			  }
+			  if($this->atm->insert_attendance_timing($data)){
+				  if($this->cm->insert_config_qatracker($data)){
+					$this->db->trans_commit();
+					return TRUE;					  						  
 				  }else{
 					$this->db->trans_rollback();
 					return FALSE;
 				  }
 			  }else{
-				  $this->db->trans_rollback();
-				  return FALSE;
+				$this->db->trans_rollback();
+				return FALSE;
 			  }
+			  
 		  }else{
 			  $this->db->trans_rollback();
 			  return FALSE;
